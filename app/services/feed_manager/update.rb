1eededd9371a367a22bc0d86703279bbfa1aed25
Davide Santangelo <davide.santangelo@gmail.com>
2023-03-10 16:16:54 +0100

    Update update.rb

--- app/services/feed_manager/update.rb
+++ app/services/feed_manager/update.rb
@@ -26,7 +26,7 @@ module FeedManager
       result = Entries.new(feed:).call
 
       result.select do |entry|
-        entry.published.present? && entry.published >= feed.items.last.published_at
+        entry.published.present? && entry.published.to_i >= feed.items.last&.published_at.to_i
       end
     end
   end
